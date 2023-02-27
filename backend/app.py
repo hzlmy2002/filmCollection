d5668289f85d87979986ec3a9bd1722e8acde45f
preston-tong25 <72134429+preston-tong25@users.noreply.github.com>
2023-02-27 21:31:22 +0000

    switch to master

--- backend/app.py
+++ backend/app.py
@@ -1,36 +1,37 @@
+import os
 from conn import dbConnection
-from database import csv2sql
+#from database import csv2sql
 from flask import Flask, render_template
 from flask_restx import Api
 from uc12 import ViewTitle, ViewDetails
 
-class DBManager:
-    def __init__(self):
-        self.connection = dbConnection
+# class DBManager:
+#     def __init__(self):
+#         self.connection = dbConnection
 
-    def populate_db(self):
-        csv2sql.load_database()
-        print("Database loaded")
+#     def populate_db(self):
+#         csv2sql.load_database()
+#         print("Database loaded")
         
-        
+
 app = Flask(__name__)
-api = Api(app)
+#api = Api(app)
 conn = None
 
 
-api.add_resource(ViewTitle, '/api/v1/view/title/<string:keyword>')
-api.add_resource(ViewDetails, '/api/v1/view/details/<int:movie_id>')
+#api.add_resource(ViewTitle, '/api/v1/view/title/<string:keyword>')
+#api.add_resource(ViewDetails, '/api/v1/view/details/<int:movie_id>')
 
-@app.route('/')
+@app.route("/")
 def movieTable():
-    global conn
-    if not conn:
-        conn = DBManager()
-        conn.populate_db
-    print("DB is connected")
-
+    # global conn
+    # if not conn:
+    #     conn = DBManager()
+    #     conn.populate_db
+    # print("DB is connected")
+    print("inside movie table")
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True,host='0.0.0.0',port=8000)
