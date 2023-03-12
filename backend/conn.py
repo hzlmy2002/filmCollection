166f76c0c436ab0d5e49279b00d6c2bb165545b6
preston-tong25 <72134429+preston-tong25@users.noreply.github.com>
2023-03-12 13:30:53 +0000

    added swagger port

--- backend/conn.py
+++ backend/conn.py
@@ -1,4 +1,4 @@
 import mysql.connector
 
-dbConnection=mysql.connector.connect(user="root",password="film",host="db",database="films",connect_timeout=5)
+dbConnection=mysql.connector.connect(user="root",password="film",host="127.0.0.1",database="films",connect_timeout=5)
 dbConnection.ping(reconnect=True)
