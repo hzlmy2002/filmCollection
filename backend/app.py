f8ef80a14fac3cf70cf487da0f20823c58a22f78
zcabvka <pun.kamthornthip.20@ucl.ac.uk>
2023-03-13 19:47:57 +0000

    Add to swagger

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from conn import dbConnection
 from flask import Flask
 from flask_restx import Api
 from uc12 import ViewTitle, ViewDetails
-from visual_browsing import GetAllGenres, GetMovieGenres
+from visual_browsing import GetAllGenres, GetMovieGenres, GetMoviesData
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,9 +10,9 @@ api = Api(app)
 
 api.add_resource(ViewTitle, '/api/v1/view/title/<string:keyword>')
 api.add_resource(ViewDetails, '/api/v1/view/details/<int:movie_id>')
-api.add_resource(GetAllGenres, '/visual-browsing/get-all-genres')
-api.add_resource(
-    GetMovieGenres, '/visual-browsing/get-movie-genres/<int:movieID>')
+api.add_resource(GetAllGenres, '/api/v1/view/all-genres')
+api.add_resource(GetMovieGenres, '/api/v1/view/movie-genres/<int:movieID>')
+api.add_resource(GetMoviesData, '/api/v1/view/movies-data')
 
 
 if __name__ == '__main__':
--- backend/visual_browsing.py
+++ backend/visual_browsing.py
@@ -1,17 +1,18 @@
 from typing import TypedDict, Tuple
 from sql_executor import SqlExecutor
-from flask_restx import Resource
+from flask import Flask
+from flask_restx import Resource, inputs, Api
 
 
-class TableSorting(TypedDict):
-    asc: bool
-    field: str
+# class TableSorting(TypedDict):
+#     asc: bool
+#     field: str
 
 
-class TableFilters(TypedDict):
-    date: Tuple[int, int]
-    genre: str
-    rating: Tuple[float, float]
+# class TableFilters(TypedDict):
+#     date: Tuple[int, int]
+#     genre: str
+#     rating: Tuple[float, float]
 
 
 class GetAllGenres(Resource):
@@ -26,37 +27,56 @@ class GetMovieGenres(Resource):
         command = ("SELECT Genres.genre "
                    "FROM Movies, Movie_Genres, Genres "
                    "WHERE Movies.movieID = Movie_Genres.movieID AND Movie_Genres.genreID = Genres.genreID ")
-        command += f"AND Movies.movieID = '{movieID}' "
+        command += f"AND Movies.movieID = \"{movieID}\" "
         result = [row[0] for row in SqlExecutor().execute_sql(command)]
         return result
 
 
+app = Flask(__name__)
+api = Api(app)
+
+parser = api.parser()
+parser.add_argument('sorting_asc', type=inputs.boolean, required=True)
+parser.add_argument('sorting_field', type=str, required=True)
+parser.add_argument('start_year', type=int)
+parser.add_argument('end_year', type=int)
+parser.add_argument('genre', type=str)
+parser.add_argument('from_rating', type=int)
+parser.add_argument('to_rating', type=int)
+
+
 class GetMoviesData(Resource):
-    def get(self, filters: TableFilters, sorting: TableSorting):
-        command = ("SELECT Movies.title, Movies.date, Genres.genre, Movies.rotten_tomatoes_rating "
+    @api.expect(parser)
+    def get(self):
+        args = parser.parse_args()
+        command = ("SELECT Movies.movieID, Movies.title, Movies.date, Genres.genre, Movies.rotten_tomatoes_rating "
                    "FROM Movies "
                    "RIGHT JOIN Movie_Genres ON Movies.movieID = Movie_Genres.movieID "
                    "LEFT JOIN Genres ON Movie_Genres.genreID = Genres.genreID ")
-        command += f"WHERE Genres.genre = '{filters['genre']}' "
-        date = filters['date']
-        rating = filters['rating']
-        if date[0] != -1:
-            command += f"AND year(Movies.date) >= {date[0]} "
-        if date[1] != -1:
-            command += f"AND year(Movies.date) <= {date[1]} "
-        if rating[0] != -1:
-            command += f"AND Movies.rotten_tomatoes_rating >= {rating[0]} "
-        if rating[1] != -1:
-            command += f"AND Movies.rotten_tomatoes_rating <= {rating[1]} "
-        sorting_mode = 'ASC' if sorting['asc'] else 'DESC'
-        command += f"\nORDER BY {sorting['field']} {sorting_mode} "
+        if args.genre is not None:
+            command += f"WHERE Genres.genre = '{args.genre}' "
+        else:
+            command += f"WHERE TRUE "
+        if args.start_year is not None:
+            command += f"AND year(Movies.date) >= {args.start_year} "
+        if args.end_year is not None:
+            command += f"AND year(Movies.date) <= {args.end_year} "
+        if args.from_rating is not None:
+            command += f"AND Movies.rotten_tomatoes_rating >= {args.from_rating} "
+        if args.to_rating is not None:
+            command += f"AND Movies.rotten_tomatoes_rating <= {args.to_rating} "
+        sorting_mode = 'ASC' if args.sorting_asc else 'DESC'
+        command += f" ORDER BY {args.sorting_field} {sorting_mode} "
 
         result = SqlExecutor().execute_sql(command)
         result_dict = SqlExecutor().convert_to_dict(
-            result, ["Title", "Date", "Genre", "Rotten_tomatoes_rating"])
+            result, ["movieID", "title", "date", "genre", "rotten_tomatoes_rating"])
         for movie in result_dict:
-            movie["Genre"] = self.get_movie_genres(movie["Title"])
-        print(result_dict)
+            if movie["date"] is not None:
+                movie["date"] = movie["date"].strftime("%m/%d/%Y")
+            else:
+                movie["date"] = ""
+            movie["genre"] = GetMovieGenres().get(movie["movieID"])
         return result_dict
 
 
