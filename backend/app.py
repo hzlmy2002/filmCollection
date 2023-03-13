67ab81f822b70bd8170c58a2d71119b11d822800
zcabvka <pun.kamthornthip.20@ucl.ac.uk>
2023-03-13 23:13:21 +0000

    Add Movie Searcher

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_restx import Api
 from uc12 import ViewTitle, ViewDetails
 from visual_browsing import GetAllGenres, GetMovieGenres, GetMoviesData
+from movie_searcher import GetMovieActors, MovieSearcher
 
 from uc3 import analyseGeneralRatingAPI, analyseRatingByGenresAPI, analyseRatingSameGenresAPI
 
@@ -15,11 +16,17 @@ api.add_resource(ViewDetails, '/api/v1/view/details/<int:movie_id>')
 api.add_resource(GetAllGenres, '/api/v1/view/all-genres')
 api.add_resource(GetMovieGenres, '/api/v1/view/movie-genres/<int:movieID>')
 api.add_resource(GetMoviesData, '/api/v1/view/movies-data')
+api.add_resource(GetMovieActors, '/api/v1/view/movie-actors/<int:movieID>')
 
-api.add_resource(analyseGeneralRatingAPI, '/api/v1/rating/general/<int:movieID>')
-api.add_resource(analyseRatingByGenresAPI, '/api/v1/rating/genres/<int:movieID>/<int:genreID>')
-api.add_resource(analyseRatingSameGenresAPI, '/api/v1/rating/samegenres/<int:movieID>')
+api.add_resource(
+    MovieSearcher, '/api/v1/search/<string:column>/<string:value>')
 
+api.add_resource(analyseGeneralRatingAPI,
+                 '/api/v1/rating/general/<int:movieID>')
+api.add_resource(analyseRatingByGenresAPI,
+                 '/api/v1/rating/genres/<int:movieID>/<int:genreID>')
+api.add_resource(analyseRatingSameGenresAPI,
+                 '/api/v1/rating/samegenres/<int:movieID>')
 
 
 if __name__ == '__main__':
