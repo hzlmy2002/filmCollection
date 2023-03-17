b8b262c4cb24870e44045483bf3abf465e6b0b05
zcabvka <pun.kamthornthip.20@ucl.ac.uk>
2023-03-17 01:35:10 +0000

    Add tag analysis by genre

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_restx import Api, Resource
 from uc12 import ViewTitle, ViewDetails
 from uc3 import analyseGeneralRatingAPI, analyseRatingByGenresAPI, analyseRatingSameGenresAPI
 from visual_browsing import GetAllGenres, GetMovieGenres, GetMoviesData
+from tag_analysis import GetTagsByGenre
 
 from uc3 import analyseGeneralRatingAPI, analyseRatingByGenresAPI, analyseRatingSameGenresAPI, analyseRatingGroupGenresAPI
 
@@ -13,27 +14,36 @@ conn = None
 
 api = Api(app, doc='/api/docs')
 
-#namespace of the API
-api_ns = api.namespace('api', description= 'Example')
+# namespace of the API
+api_ns = api.namespace('api', description='Example')
 
-#UC 1 and UC 2
+# UC 1 and UC 2
 api.add_resource(ViewTitle, '/api/v1/view/title/<string:keyword>')
 api.add_resource(ViewDetails, '/api/v1/view/details/<int:movie_id>')
 api.add_resource(GetAllGenres, '/api/v1/view/all-genres')
 api.add_resource(GetMovieGenres, '/api/v1/view/movie-genres/<int:movieID>')
 api.add_resource(GetMoviesData, '/api/v1/view/movie-data')
 
-#UC 3
-api.add_resource(analyseGeneralRatingAPI, '/api/v1/rating/general/<int:movieID>')
-api.add_resource(analyseRatingByGenresAPI, '/api/v1/rating/genres/<int:movieID>/<int:genreID>')
-api.add_resource(analyseRatingSameGenresAPI, '/api/v1/rating/samegenres/<int:movieID>')
+# UC 3
+api.add_resource(analyseGeneralRatingAPI,
+                 '/api/v1/rating/general/<int:movieID>')
+api.add_resource(analyseRatingByGenresAPI,
+                 '/api/v1/rating/genres/<int:movieID>/<int:genreID>')
+api.add_resource(analyseRatingSameGenresAPI,
+                 '/api/v1/rating/samegenres/<int:movieID>')
 
-api.add_resource(analyseGeneralRatingAPI, '/api/v1/rating/general/<int:movieID>')
-api.add_resource(analyseRatingByGenresAPI, '/api/v1/rating/genres/<int:movieID>/<int:genreID>')
-api.add_resource(analyseRatingSameGenresAPI, '/api/v1/rating/samegenres/<int:movieID>')
-api.add_resource(analyseRatingGroupGenresAPI, '/api/v1/rating/manygenres/<int:movieID>')
+api.add_resource(analyseGeneralRatingAPI,
+                 '/api/v1/rating/general/<int:movieID>')
+api.add_resource(analyseRatingByGenresAPI,
+                 '/api/v1/rating/genres/<int:movieID>/<int:genreID>')
+api.add_resource(analyseRatingSameGenresAPI,
+                 '/api/v1/rating/samegenres/<int:movieID>')
+api.add_resource(analyseRatingGroupGenresAPI,
+                 '/api/v1/rating/manygenres/<int:movieID>')
 
+# UC4
+api.add_resource(GetTagsByGenre, '/api/v1/tags/genre/<string:genre>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
